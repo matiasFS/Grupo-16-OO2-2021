e9e1b51e2f1dc97153f0ac99b2269b42e5bbd4ac
Matias Silvestri <matias.fsilvestri@gmail.com>
2021-05-27 12:27:08 -0300

    Agregue repositorio y service de permisos

---
+++ src/main/java/com/trabajo/Grupo16OO22021/services/IPermisoService.java
@@ -0,0 +1,23 @@
+package com.trabajo.Grupo16OO22021.services;
+
+import java.util.List;
+
+import com.trabajo.Grupo16OO22021.entities.PermisoDiario;
+import com.trabajo.Grupo16OO22021.entities.PermisoPeriodo;
+import com.trabajo.Grupo16OO22021.models.PermisoDiarioModel;
+import com.trabajo.Grupo16OO22021.models.PermisoPeriodoModel;
+
+public interface IPermisoService {
+
+	public List<PermisoPeriodo> getAll();
+	
+	
+	public List<PermisoDiario> getAll1();
+
+	
+	public PermisoPeriodoModel insertOrUpdate(PermisoPeriodoModel permisoPeriodoModel);
+	
+	public PermisoDiarioModel insertOrUpdate(PermisoDiarioModel permisoDiarioModel);
+
+	public PermisoPeriodoModel findById(int id);
+}
---
+++ src/main/java/com/trabajo/Grupo16OO22021/services/implementation/PermisoService.java
@@ -0,0 +1,72 @@
+package com.trabajo.Grupo16OO22021.services.implementation;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+import com.trabajo.Grupo16OO22021.converters.PermisoConverter;
+import com.trabajo.Grupo16OO22021.entities.PermisoDiario;
+import com.trabajo.Grupo16OO22021.entities.PermisoPeriodo;
+import com.trabajo.Grupo16OO22021.models.PermisoDiarioModel;
+import com.trabajo.Grupo16OO22021.models.PermisoPeriodoModel;
+import com.trabajo.Grupo16OO22021.repositories.IPermisoDiarioRepository;
+import com.trabajo.Grupo16OO22021.repositories.IPermisoPeriodoRepository;
+import com.trabajo.Grupo16OO22021.services.IPermisoService;
+
+@Qualifier("permisoService")
+@Service
+public class PermisoService implements IPermisoService {
+
+	
+	@Autowired
+	@Qualifier("permisoPeriodoRepository")
+	private IPermisoPeriodoRepository permisoPeriodoRepository;
+	
+	@Autowired
+	@Qualifier("permisoDiarioRepository")
+	private IPermisoDiarioRepository permisoDiarioRepository;
+
+	@Autowired
+	@Qualifier("permisoConverter")
+	private PermisoConverter permisoConverter;
+	
+	
+	@Override
+	public List<PermisoPeriodo> getAll() {
+		return permisoPeriodoRepository.findAll();
+	}
+	@Override
+	public List<PermisoDiario> getAll1() {
+		return permisoDiarioRepository.findAll();
+	}
+
+	@Override
+	public PermisoPeriodoModel insertOrUpdate(PermisoPeriodoModel permisoPeriodoModel) {
+	
+		PermisoPeriodo permiso = permisoPeriodoRepository.save(permisoConverter.modelToEntity(permisoPeriodoModel));
+		permiso = permisoPeriodoRepository.save(permiso);
+		
+	return  permisoConverter.entityToModel(permiso);
+	}
+
+
+	@Override
+	public PermisoPeriodoModel findById(int id) {
+		return permisoConverter.entityToModel(permisoPeriodoRepository.findById(id));
+	}
+
+	@Override
+	public PermisoDiarioModel insertOrUpdate(PermisoDiarioModel permisoDiarioModel) {
+		
+		PermisoDiario permiso = permisoDiarioRepository.save(permisoConverter.permisoDiarioModelToEntity(permisoDiarioModel));
+		permiso = permisoDiarioRepository.save(permiso);
+		
+	return  permisoConverter.permisoDiarioEntityToModel(permiso);
+	}
+
+	
+
+
+}
