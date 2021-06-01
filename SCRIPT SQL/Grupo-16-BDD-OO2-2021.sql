33a925182123e4d05dc4e502b1776ad76d305a08
Matias Silvestri <matias.fsilvestri@gmail.com>
2021-06-01 20:23:41 -0300

    Modifiqué el sql

--- SCRIPT SQL/Grupo-16-BDD-OO2-2021.sql
+++ SCRIPT SQL/Grupo-16-BDD-OO2-2021.sql
@@ -8,11 +8,14 @@ insert into user_role values(2,'2021-05-13', 'ROLE_AUDITOR','AUDITOR','2021-05-1
 insert into user values(1, 11111111, 'admin@gmail.com', true, 'admin', 'admin','$2a$10$qoqhe.Nqr9jNnKsXdBWZ3edShJHwkbJni4dnbHHnpSMiFdKm5hZUW','admin',1);
 insert into user values(2, 22222222, 'auditor@gmail.com', true, 'auditor', 'auditor','$2a$10$B9mfALrCwl7gNgksyajanuqhq2GpRAfcfTPN9iXrAAGN6BKB73hUy','auditor',2);
 
-insert into lugar values(1, '1832', 'Lomas de Zamora');
+insert into lugar values(1, '1832', 'LOMAS DE ZAMORA');
 insert into lugar values(2, '1675', 'CABA');
-insert into lugar values(3, '7600', 'Mar del plata');
-insert into lugar values(4, '1900', 'La Plata');
-insert into lugar values(5, '1841', 'Monte Grade');
+insert into lugar values(3, '7600', 'MAR DEL PLATA');
+insert into lugar values(4, '1900', 'LA PLATA');
+insert into lugar values(5, '1841', 'MONTE GRANDE');
 
+select * from persona;
 select * from rodado;
 select * from permiso;
+select * from permiso_periodo;
+select * from permiso_diario;
