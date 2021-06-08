179007eddbaa72445d74df82083b7a545c17f09a
jonaaa12 <jonathan.a.chaves@gmail.com>
2021-06-07 23:32:25 -0300

    Data sql, index y Controller del QR

--- src/main/java/com/trabajo/Grupo16OO22021/controllers/QRCodeController.java
+++ src/main/java/com/trabajo/Grupo16OO22021/controllers/QRCodeController.java
@@ -37,15 +37,41 @@ public class QRCodeController {
 	@GetMapping("/qr-code{id}")
 	public ResponseEntity<byte[]> getQrCode(@PathVariable("id") int id) {
 		Optional<PermisoPeriodo> permiso = permisoPeriodoRepository.findById(id);
-		byte[] qrImage = qrCodeService.generate(permiso.toString(), WIDTH, HEIGHT);
 
+		String link = "https://mattc7.github.io/Grupo-16-OO2-2021/index?";
+
+		String fecha = "fecha=" + permiso.get().getFecha();
+
+		String pedido = "&documento=" + permiso.get().getPedido().getDocumento() + "&nombre="
+				+ permiso.get().getPedido().getNombre() + "&apellido=" + permiso.get().getPedido().getApellido();
+		
+		String rodado = "&dominio=" + permiso.get().getRodado().getDominio();
+		
+		String cantDiasYVacaciones = "&cantDias=" + permiso.get().getCantDias() + "&vacaciones="
+				+ permiso.get().isVacaciones();
+		
+		String informacion = link + fecha + pedido + rodado + cantDiasYVacaciones;
+	
+		byte[] qrImage = qrCodeService.generate(informacion, WIDTH, HEIGHT);
 		return ResponseEntity.ok().contentType(MediaType.IMAGE_PNG).body(qrImage);
 	}
 	
 	@GetMapping("/qrcode{id}")
 	public ResponseEntity<byte[]> getQrCode1(@PathVariable("id") int id) {
 		Optional<PermisoDiario> permiso = permisoDiarioRepository.findById(id);
-		byte[] qrImage = qrCodeService.generate(permiso.toString(), WIDTH, HEIGHT);
+		String link = "https://mattc7.github.io/Grupo-16-OO2-2021/index?";
+
+		String fecha = "fecha=" + permiso.get().getFecha();
+
+		String pedido = "&documento=" + permiso.get().getPedido().getDocumento() + "&nombre="
+				+ permiso.get().getPedido().getNombre() + "&apellido=" + permiso.get().getPedido().getApellido();
+			
+		String motivo = "&motivo=" + permiso.get().getMotivo();
+		
+		String informacion = link + fecha + pedido + motivo;
+				
+		
+		byte[] qrImage = qrCodeService.generate(informacion, WIDTH, HEIGHT);
 
 		return ResponseEntity.ok().contentType(MediaType.IMAGE_PNG).body(qrImage);
 	}
